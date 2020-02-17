83af3d4b3c0b428102cfb997491f6898c7267a60
Sales, Anderson C <anderson.c.sales@accenture.com>
2020-02-17 17:29:31 -0300

    Teste distribuição
    Teste distribuição.

--- app/src/main/java/com/accenture/poccodepush/MainActivity.kt
+++ app/src/main/java/com/accenture/poccodepush/MainActivity.kt
@@ -2,11 +2,18 @@ package com.accenture.poccodepush
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.distribute.Distribute
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application,
+            "9ad9f9fb-0374-4906-9488-44702428f84e",
+            Distribute::class.java)
     }
 }
